f491c2fc437f0e1f40c9d0f9d13879ab08c26c65
fubaozhang <15218444@qq.com>
2018-11-15 16:19:44 +0800

    初学者，参考vue-admin-master的登录界面编写

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+/* import HelloWorld from '@/components/HelloWorld' */
 import test from '@/components/test'
+import login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
     },
     {
       path: '/day01',
